81771a80c9a5de38542bb931b0d4edda0612b5db
ClaraMacedo <catitaanaclara@gmail.com>
2017-09-29 15:09:44 -0300

    commit  2 - 29/09/2017 branch update
    calculando medias corretamente e mostranto a situação do aluno corretamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
     title: string;
     alunos: Array<Aluno>;
     selecionado: Aluno;
-    novo: Aluno = new Aluno('',null, null, null,null, null, null, null, "N/I");
+    novo: Aluno = new Aluno('',null, 0, null,null, null, null, null, "N/I");
     editando = false;
     mediando: number;
     cont= 0;
@@ -21,11 +21,11 @@ export class AppComponent implements OnInit {
     constructor() {
         this.title = 'Alunos';
         this.alunos = [
-            new Aluno('Carol Danvers', 1001, null, null, null, null, null, null, ''),
-            new Aluno('Clint Barton', 1001, 10, 10, 10, 10, null, null, ''),
-            new Aluno('Natasha Romanoff', 1001, null, null, null, null, null, null, ''),
-            new Aluno('Steve Rogers', 1001, null, null, null, null, null, null, ''),
-            new Aluno('Wade Wilson', 1001, 10, 10, 10, 10, null, null, '')
+            new Aluno('Carol Danvers', 1001, null, null, null, null, null, null, "N/I"),
+            new Aluno('Clint Barton', 1001, 10, 10, 10, 10, null, null, "N/I"),
+            new Aluno('Natasha Romanoff', 1001, null, null, null, null, null, null, "N/I"),
+            new Aluno('Steve Rogers', 1001, null, null, null, null, null, null, "N/I"),
+            new Aluno('Wade Wilson', 1001, 10, 10, 10, 10, null, null, "N/I")
         ];
     }
 
@@ -52,11 +52,11 @@ export class AppComponent implements OnInit {
     cadastrar(): void {
         if(!this.editando){
             this.alunos.push(new Aluno(this.novo.nome, this.novo.turma, this.novo.bim1, this.novo.bim2, this.novo.bim3, this.novo.bim4, this.novo.media, this.novo.frequencia, this.novo.situacaoFinal));
-            this.novo = new Aluno('',null, null, null,null, null, null, null, "N/I");;
+            this.novo = new Aluno('',null, null, null,null, null, null, null, "N/I");
             console.log("Com inclusão de alunos...");
         }
         else{
-            this.novo = new Aluno('',null, null, null,null, null, null, null, "N/I");;
+            this.novo = new Aluno('',null, null, null,null, null, null, null, "N/I");
             this.editando = false;
             console.log("Sem inclusão de alunos...");
         }
@@ -78,6 +78,11 @@ export class AppComponent implements OnInit {
         let indice = -1;
         let mediaTurma =0;
         let contando = 0;
+        let cont = 0;
+        let mediando = 0;
+        var resum:number = 0;
+        var sum = (number1: number, number2: number) =>  number1 + number2;
+
         for (let i=0; i< this.alunos.length; i++){
             if(this.alunos[i].bim1 == null && this.alunos[i].bim2 == null && this.alunos[i].bim3 == null && this.alunos[i].bim4 == null){
                 this.alunos[i].media = null;
@@ -86,32 +91,37 @@ export class AppComponent implements OnInit {
                 }
             }
             else{
-                if(this.alunos[i].bim1 != null){
-                    this.mediando += this.alunos[i].bim1;
-                    this.cont += 1;
+                if(this.alunos[i].bim1 != null || this.alunos[i].bim1 == 0){
+                    //mediando = mediando + this.alunos[i].bim1;
+                    resum = sum(this.alunos[i].bim1.valueOf() , resum);
+                    cont += 1;
                 }
-                if(this.alunos[i].bim2 != null){
-                    this.mediando += this.alunos[i].bim2;
-                    this.cont += 1;
+                if(this.alunos[i].bim2 != null || this.alunos[i].bim2 == 0){
+                    //mediando = mediando + this.alunos[i].bim2;
+                    resum = sum(this.alunos[i].bim2.valueOf() , resum);
+                    cont += 1;
                 }
-                if(this.alunos[i].bim3 != null){
-                    this.mediando += this.alunos[i].bim3;
-                    this.cont += 1;
+                if(this.alunos[i].bim3 != null || this.alunos[i].bim3 == 0){
+                    //mediando = mediando + this.alunos[i].bim3;
+                    resum = sum(this.alunos[i].bim3.valueOf() , resum);
+                    cont += 1;
                 }
-                if(this.alunos[i].bim4 != null){
-                    this.mediando += this.alunos[i].bim4;
-                    this.cont += 1;
+                if(this.alunos[i].bim4 != null || this.alunos[i].bim4 == 0){
+                    //mediando = mediando + this.alunos[i].bim4;
+                    resum = sum(this.alunos[i].bim3.valueOf() , resum);
+                    cont += 1;
                 }
-                this.alunos[i].media = this.mediando/this.cont;
+                this.alunos[i].media = resum/cont;
                 mediaTurma+= this.alunos[i].media;
+                
                 console.log("Calculando media aluno...", this.alunos[i].media, mediaTurma);
             }                
             contando +=1;
-            this.mediando = 0;
-            this.cont = 0;    
+            mediando = 0;
+            cont = 0;
+            resum = 0;    
             console.log("Calculando...");
-            
-    }
+        }
         this.mediaDaTurma = mediaTurma/contando;
         console.log("Calculando media turma...", this.mediaDaTurma);           
        
@@ -120,7 +130,7 @@ export class AppComponent implements OnInit {
     mostrarSituacao(): void{
         for (let i=0; i< this.alunos.length; i++){
             const fre = 200;
-            if(this.alunos[i].frequencia <= (fre * 75/100)){
+            if(this.alunos[i].frequencia <= (fre * 75/100) && this.alunos[i].frequencia != null){
                 this.alunos[i].situacaoFinal= "REPROVADO POR FALTA";
             }
             else{
@@ -128,7 +138,13 @@ export class AppComponent implements OnInit {
                 this.alunos[i].situacaoFinal= "APROVADO";
                 }
                 else{
-                    this.alunos[i].situacaoFinal= "REPROVADO";
+                    if(this.alunos[i].media < 7 && this.alunos[i].media != null)
+                    {
+                     this.alunos[i].situacaoFinal= "REPROVADO";
+                    }
+                    else{
+                        this.alunos[i].situacaoFinal= "N/I";
+                    }
                 }
             }
             
